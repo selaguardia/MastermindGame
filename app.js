3578d16b7a356599d0516389fbc74bffb34eb3dc
selaguardia <selaguardia91@gmail.com>
2022-05-30 11:34:29 -0700

    adds game object, dynamic text, and event handlers for begining and resetting the game

---
+++ app.js
@@ -0,0 +1,33 @@
+const message = document.getElementById('messageBox');
+const button = document.getElementById('checkButton');
+const playerHistory = document.getElementById("playerHistory");
+const playerInput = document.querySelectorAll('.playerInput');
+
+let randomNums = [];
+let playerGuesses = [];
+let correctNums = 0;
+let correctNumsAndPos = 0;
+const attempts = 10;
+// Game object
+const game = {
+  attempts,
+  num: 4
+}
+
+// Adds dynamic text for button and  instructions
+message.textContent = "Click the button below to start game!";
+button.textContent = "Start Game";
+
+// Button click handler
+button.addEventListener('click', (e) => {
+  if(button.textContent === 'Start Game') {
+    // Reset game
+    game.attempts = attempts;
+    playerHistory.innerHTML='';
+    randomNums=[];
+    playerGuesses=[];
+    button.textContent = "Unlock";
+    message.textContent = "Guess the 4-digit combo to unlock the prize";
+  }
+});
+
